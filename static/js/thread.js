367056a024dffd9ca8c097c1a7438ebb3e310505
Lain Iwakura <90155169+lainsec@users.noreply.github.com>
2024-09-27 01:15:05 +0000

    No fap september ta quase terminando

--- static/js/thread.js
+++ static/js/thread.js
@@ -19,6 +19,49 @@ function substituirSpoilers() {
         pre.innerHTML = pre.innerHTML.replace(/\[r\](.*?)\[\/r\]/g, '<span class="rainbowtext">$1</span>');
     }
 }
+const textarea = document.getElementById('text');
+
+let quoteButton;
+
+    document.addEventListener('mouseup', () => {
+        const selection = window.getSelection();
+        if (selection.toString()) {
+            if (!quoteButton) {
+                quoteButton = document.createElement('button');
+                quoteButton.className = 'quote-button';
+                quoteButton.innerText = 'Quotar';
+                document.body.appendChild(quoteButton); 
+            }
+
+            const range = selection.getRangeAt(0);
+            const rect = range.getBoundingClientRect();
+
+            quoteButton.style.left = `${rect.left + window.scrollX}px`;
+            quoteButton.style.top = `${rect.top + window.scrollY - 10}px`;
+
+            requestAnimationFrame(() => {
+                quoteButton.style.display = 'block'; 
+            });
+        } else if (quoteButton) {
+            quoteButton.style.display = 'none'; 
+        }
+    });
+
+    document.addEventListener('click', (e) => {
+        if (quoteButton && e.target !== quoteButton) {
+            quoteButton.style.display = 'none';
+        }
+    });
+
+    document.addEventListener('click', (e) => {
+        if (quoteButton && e.target === quoteButton) {
+            const selection = window.getSelection();
+            const selectedText = selection.toString();
+            textarea.value += '>' + (textarea.value ? '\n' : '') + selectedText;
+            quoteButton.style.display = 'none';
+            window.getSelection().removeAllRanges();
+        }
+    });
 window.onload = function() {
     substituirSpoilers();
 };
