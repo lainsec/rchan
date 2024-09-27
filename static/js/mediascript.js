8e636d7b711769c095cfcd775fb026e2fe874c64
Lain Iwakura <90155169+lainsec@users.noreply.github.com>
2024-09-27 01:15:36 +0000

    No fap september ta quase terminando

--- static/js/mediascript.js
+++ static/js/mediascript.js
@@ -15,3 +15,34 @@ postImgs.forEach(function(img) {
         }
     });
 });
+
+const postImages = document.querySelectorAll('img.post_img');
+const zoomedImage = document.createElement('img');
+zoomedImage.className = 'zoomed-image';
+zoomedImage.style.position = 'absolute';
+zoomedImage.style.display = 'none'; 
+
+document.body.appendChild(zoomedImage);
+
+postImages.forEach(image => {
+  image.addEventListener('mouseover', function(event) {
+    zoomedImage.src = image.src;
+    zoomedImage.style.display = 'block'; 
+    updateZoomedImagePosition(event);
+  });
+
+  image.addEventListener('mousemove', function(event) {
+    updateZoomedImagePosition(event);
+  });
+
+  image.addEventListener('mouseout', function() {
+    zoomedImage.style.display = 'none';
+  });
+});
+
+function updateZoomedImagePosition(event) {
+  const offsetX = 20; 
+  const offsetY = 20; 
+  zoomedImage.style.top = (event.clientY + offsetY) + 'px';
+  zoomedImage.style.left = (event.clientX + offsetX) + 'px';
+}
