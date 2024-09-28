4463adf187ec079b5c424f5332618f7e4694300a
Lain Iwakura <90155169+lainsec@users.noreply.github.com>
2024-09-28 12:46:35 +0900

    YEs

--- static/js/postform.js
+++ static/js/postform.js
@@ -2,8 +2,34 @@ document.getElementById('togglePostFormLink').addEventListener('click', function
     event.preventDefault();
     var formDiv = document.querySelector('.newboard-form');
     formDiv.style.display = formDiv.style.display === 'none'? 'block' : 'none';
-    
-    if (formDiv.style.display === 'block') {
-        document.getElementById('togglePostFormLink').style.display = 'none';
-    }
+
 });
+
+const draggableForm = document.getElementById('draggableForm');
+
+draggableForm.addEventListener('mousedown', function(e) {
+    let shiftX = e.clientX - draggableForm.getBoundingClientRect().left;
+    let shiftY = e.clientY - draggableForm.getBoundingClientRect().top;
+
+    function moveAt(pageX, pageY) {
+        draggableForm.style.left = pageX - shiftX + 'px';
+        draggableForm.style.top = pageY - shiftY + 'px';
+    }
+
+    function onMouseMove(e) {
+        moveAt(e.pageX, e.pageY);
+    }
+
+    document.addEventListener('mousemove', onMouseMove);
+
+    function onMouseUp() {
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp); 
+    }
+
+    document.addEventListener('mouseup', onMouseUp);
+});
+
+draggableForm.ondragstart = function() {
+    return false;
+};
