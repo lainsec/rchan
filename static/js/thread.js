e97aaa590fbb0a569ce8d7493b2b4844ba169316
Lain Iwakura <90155169+lainsec@users.noreply.github.com>
2024-10-12 18:38:25 +0000

    New features

--- static/js/thread.js
+++ static/js/thread.js
@@ -1,15 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var postContents = document.querySelectorAll('.post_content pre');
+    var postContents = document.querySelectorAll('pre');
     postContents.forEach(function(postContent) {
-        postContent.innerHTML = postContent.innerHTML.replace(/&gt;([^<&\n]+)/g, '<span class="verde">&gt;$1</span>')
+        postContent.innerHTML = postContent.innerHTML.replace(/&gt;([^<&\n]+)/g, '<span class="verde">&gt;$1</span>');
         postContent.innerHTML = postContent.innerHTML.replace(/&lt;([^<&\n]+)/g, '<span class="vermelho">&lt;$1</span>');
+        postContent.innerHTML = postContent.innerHTML.replace(/\(\(\(([^()]*?)\)\)\)/g, '<span class="detected">((( $1 )))</span>');
+        postContent.innerHTML = postContent.innerHTML.replace(/==([^=]+)==/g, '<span class="red-text">$1</span>');
     });
-    var replyContents = document.querySelectorAll('.reply_content pre');
-    replyContents.forEach(function(replyContent) {
-        replyContent.innerHTML = replyContent.innerHTML.replace(/&gt;([^<&\n]+)/g, '<span class="verde">&gt;$1</span>');
-        replyContent.innerHTML = replyContent.innerHTML.replace(/&lt;([^<&\n]+)/g, '<span class="vermelho">&lt;$1</span>');
-        replyContent.innerHTML = replyContent.innerHTML.replace(/(?:^|\s)(#[^\s<\n]+)/g, '<a class="vermelho-reply" href="#" style="text-decoration: none;">$1</a>');
-        });
+
 });
 function substituirSpoilers() {
     var pres = document.getElementsByTagName('pre');
