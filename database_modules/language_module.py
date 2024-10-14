a6c7c81204219251d6ac70c8cc0e29be7fe380f6
Lain Iwakura <90155169+lainsec@users.noreply.github.com>
2024-10-14 23:39:26 +0000

    Multi language.

--- database_modules/language_module.py
+++ database_modules/language_module.py
@@ -8,12 +8,28 @@ def load_langs():
     except:
         print('Ocorreu um erro ao carregar a base de dados.')
 
+def save_new_lang(lang):
+    with open('./config/languages.json', 'w') as f:
+        json.dump(lang, f, indent=4)
+
+def change_general_language(new_lang):
+    languages = load_langs()
+    if new_lang in languages:
+        lang_content = languages[new_lang]
+        if 'default' in languages:
+            del languages['default']
+        languages["default"] = lang_content
+        save_new_lang(languages)
+        return True
+    else:
+        return False
+
 def get_user_lang(user_lang):
     langs = load_langs()
     if user_lang in langs:
         return langs[user_lang][0]
     else:
-        return langs["eng-us"][0]
+        return langs["default"][0]
 
 if __name__ == '__main__':
     print('dont open this file alone.')
