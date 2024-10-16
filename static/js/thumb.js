2d1d24ac389bd9da4bbf77256cebd24e6da93e27
Lain Iwakura <90155169+lainsec@users.noreply.github.com>
2024-10-16 00:52:04 +0000

    videothumb.

---
+++ static/js/thumb.js
@@ -0,0 +1,19 @@
+const video = document.getElementById('lastpost_video');
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+
+  video.addEventListener('loadedmetadata', function() {
+    video.currentTime = 2;
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    const imageData = canvas.toDataURL();
+
+    const img = document.createElement('img');
+    img.src = imageData;
+
+    document.body.appendChild(img);
+
+    canvas.remove();
+});
