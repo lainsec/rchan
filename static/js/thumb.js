6b94e92af33c28553c4df236634f83404c63a872
Lain Iwakura <90155169+lainsec@users.noreply.github.com>
2024-12-03 22:31:30 +0900

    Video thumbnail.

--- static/js/thumb.js
+++ static/js/thumb.js
@@ -21,3 +21,31 @@ videos.forEach(video => {
         });
     });
 });
+document.addEventListener('DOMContentLoaded', function() {
+    const videos = document.querySelectorAll('.video');
+    const canvases = document.querySelectorAll('.video-thumbnail');
+    const videoPlayers = document.querySelectorAll('.post_video');
+    
+    videos.forEach(function(video, index) {
+      const canvas = canvases[index]; 
+      const videoPlayer = videoPlayers[index]; 
+  
+      // Espera o vídeo carregar os dados
+      video.addEventListener('loadeddata', function() {
+        video.currentTime = 1;
+      });
+      video.addEventListener('seeked', function() {
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height); 
+      });
+  
+      canvas.addEventListener('click', function() {
+        canvas.style.display = 'none'; 
+        videoPlayer.style.display = 'block';  
+        videoPlayer.play(); 
+      });
+    });
+});
