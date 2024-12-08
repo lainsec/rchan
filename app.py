5cbffc079f226353bfd60f0a76c9e51374ba8228
Lain Iwakura <90155169+lainsec@users.noreply.github.com>
2024-12-08 23:39:04 +0900

    Code documentation.

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+#imports.
 from flask import Flask
 from werkzeug.middleware.proxy_fix import ProxyFix
 from flask_socketio import SocketIO
 from blueprints.posts_bp import posts_bp
 from blueprints.boards_bp import boards_bp
 from blueprints.auth_bp import auth_bp
-
+#app configuration.
 app = Flask(__name__)
 socketio = SocketIO(app)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1)
@@ -15,4 +16,5 @@ app.register_blueprint(boards_bp)
 app.register_blueprint(auth_bp)
 
 if __name__ == '__main__':
+    #run with socketIO for real-time features.
     socketio.run(app, port=3000, debug=True)
