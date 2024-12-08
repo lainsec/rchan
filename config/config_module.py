cd671e9a51eac316f76e75884d22567e6c7a2111
Lain Iwakura <90155169+lainsec@users.noreply.github.com>
2024-12-09 00:05:42 +0900

    Code documentation.

--- config/config_module.py
+++ config/config_module.py
@@ -1,6 +1,7 @@
+#imports
 from flask import session, request
 from database_modules import database_module
-
+#check if user is banned
 def check_banned_user():
     session["user_ip"] = request.remote_addr
     if database_module.check_banned_user(session["user_ip"]):
