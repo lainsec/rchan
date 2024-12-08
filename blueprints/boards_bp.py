9fa76c192083556b13cf83d4aaed29c72634af87
Lain Iwakura <90155169+lainsec@users.noreply.github.com>
2024-12-08 23:58:45 +0900

    Code documentation.

--- blueprints/boards_bp.py
+++ blueprints/boards_bp.py
@@ -1,30 +1,31 @@
+#imports
 from flask import current_app, Blueprint, render_template, session, redirect, request, url_for
 from database_modules import database_module
 from database_modules import language_module
-
+#blueprint register.
 boards_bp = Blueprint('boards', __name__)
-
+#load language.
 @boards_bp.context_processor
 def inject_lang():
     lang = language_module.get_user_lang('default')
     return dict(lang=lang)
-
+#error handling.
 @boards_bp.errorhandler(404)
 def page_not_found(e):
     return redirect(url_for('boards.main_page'))
-
+#landing page route.
 @boards_bp.route('/')
 def main_page():
     posts = database_module.load_db()
     boards = database_module.load_boards()
     return render_template('index.html',boards=boards,all_posts=posts,posts=reversed(posts[-6:]))
-
+#boards page route.
 @boards_bp.route('/tabuas')
 def tabuas():
     boards = database_module.load_boards()
     posts = database_module.load_db()
     return render_template('tabuas.html',all_posts=posts,boards=boards)
-
+#account dashboard and login route.
 @boards_bp.route('/conta')
 def login():
     if 'username' in session:
@@ -33,7 +34,7 @@ def login():
         roles = database_module.get_user_role(session["username"])
         return render_template('dashboard.html', username=session["username"],roles=roles,user_boards=user_boards, all_boards=all_boards)
     return render_template('login.html')
-
+#register page route.
 @boards_bp.route('/registrar')
 def register():
     if 'username' in session:
@@ -42,7 +43,7 @@ def register():
     session['captcha_text'] = captcha_text
     print(session['captcha_text'])
     return render_template('register.html',captcha_image=captcha_image)
-
+#board creation page route.
 @boards_bp.route('/create')
 def create():
     if 'username' in session:
@@ -51,7 +52,7 @@ def create():
         return render_template('board-create.html',captcha_image=captcha_image)
     else:
         return redirect('/conta')
-
+#board page endpoint
 @boards_bp.route('/<board_uri>/')
 def board_page(board_uri):
     if not database_module.check_board(board_uri):
@@ -74,7 +75,7 @@ def board_page(board_uri):
     if 'username' in session:
         roles = database_module.get_user_role(session["username"])
     return render_template('board.html',captcha_image=captcha_image, page=page, posts_per_page=posts_per_page, pinneds=pinneds, posts=posts,replies=replies,board_banner=board_banner,board_id=board_uri,board_info=board_info, post_mode=post_mode, roles=roles)
-
+#board banners page route.
 @boards_bp.route('/<board_uri>/banners')
 def board_banners(board_uri):
     if not database_module.check_board(board_uri):
@@ -87,7 +88,7 @@ def board_banners(board_uri):
     board_banner = database_module.get_board_banner(board_uri)
     banners = database_module.get_all_banners(board_uri)
     return render_template('board_banners.html',username=username,board_banner=board_banner,banners=banners,board_id=board_uri,board_info=board_info)
-
+#thread page route.
 @boards_bp.route('/<board_name>/thread/<thread_id>')
 def replies(board_name, thread_id):
     board_id = board_name
