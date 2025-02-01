200a603fb4975721488b11a13c19f34bc3fd43c0
Lain Iwakura <90155169+lainsec@users.noreply.github.com>
2025-02-01 01:23:22 -0300

    New script added, resize images.

---
+++ static/js/mediascript.js
@@ -0,0 +1,20 @@
+const images = document.querySelectorAll('.post_img, .reply_img');
+
+images.forEach(image => {
+    image.addEventListener('click', function() {
+        this.style.display = 'none';
+
+        const newImage = document.createElement('img');
+        newImage.classList = "post_img_resized";
+        newImage.src = this.src;
+        newImage.alt = this.alt;
+        newImage.style.cursor = 'pointer';
+
+        this.parentNode.insertBefore(newImage, this.nextSibling);
+
+        newImage.addEventListener('click', function() {
+            this.remove();
+            image.style.display = '';
+        });
+    });
+});
